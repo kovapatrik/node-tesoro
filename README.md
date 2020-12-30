83544e1a7480dda64d19203c22c43dcd8a5ec333
kovapatrik <kovapatrik@gmail.com>
2020-12-30 12:21:32 +0100

    api done

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A very early project for controlling Tesoro GRAM Spektrum SE through node.js
+A very early project for controlling Tesoro GRAM Spectrum SE through node.js
--- src/app.ts
+++ src/app.ts
@@ -1,59 +1,160 @@
-import USB from "usb";
+import HID from "node-hid";
 import * as spectrum from "./spectrum";
 import * as utils from "./utils";
 import * as profile from "./profile"
+import inquirer from 'inquirer'
+
+interface ProfileState{
+    profile_num?: profile.ProfileSelect
+    r?: number
+    g?: number
+    b?: number
+    effect?: profile.Effect
+    brightness?: profile.Brightness
+    effect_color?: profile.EffectColor
+};
 
 class TesoroGramSE {
-    keyboard: USB.Device;
-    profile_num: profile.ProfileSelect;
-
-    constructor(keyboard : USB.Device) {
+    keyboard: HID.HID;
+    profile_state : ProfileState;
+    constructor(keyboard : HID.HID) {
         this.keyboard = keyboard;
-        this.profile_num = profile.ProfileSelect.NONE;
+        this.profile_state = {
+            profile_num: profile.ProfileSelect.Profile1,
+            r: 243,
+            g: 152,
+            b: 0,
+            effect: profile.Effect.Standard,
+            brightness: profile.Brightness.B100,
+            effect_color: profile.EffectColor.Static
+        }
     }
 
     async changeProfile(profile_num: profile.ProfileSelect) {
-        if (profile_num == profile.ProfileSelect.NONE) {
+        if (profile_num === undefined) {
             console.error("You can't change the profile to NONE"!);
         } else {
-            let packet = profile.PACKET_CHANGE_PROFILE.map((item) => {return item == -1 ? profile_num : item;});
-            this.profile_num = profile_num;
-            await this.sendCommand(utils.numArrayToHexString(packet), 'setProfile');
+            let packet = profile.PACKET_CHANGE_PROFILE.map((item) => {return item == 'profile' ? profile_num : item;});
+            this.profile_state.profile_num = profile_num;
+            await this.sendCommand(utils.packetToByteArray(packet), 'setProfile');
         }
     }
 
-    async setProfileSettings() {
-        if (this.profile_num == profile.ProfileSelect.NONE) {
-            console.error('Set a profile first!');
+    async setProfileSettings(data: ProfileState) {
+        if (this.profile_state.profile_num === undefined) {
+            console.error('Profile cannot be set or is undefined');
+        } else if (!Object.values(data).every(x => x != undefined)){
+            console.error("Can't set properties to undefined");
         } else {
-            let init_packet = profile.PACKET_INIT_CHANGE_PROFILE_SETTINGS.map((item) => {return item == -1 ? this.profile_num : item;});
-        }
+            let init_packet = utils.PACKET_INIT.map((item) => {return item == 'profile' ? this.profile_state.profile_num : item;});
+            await this.sendCommand(utils.packetToByteArray(init_packet), 'initPacket');
+            if (this.profile_state.profile_num != profile.ProfileSelect.PC) {
+                let middle_packet = utils.PACKET_MIDDLE.map((item) => {return item == 'profile' ? this.profile_state.profile_num : item;});
+                await this.sendCommand(utils.packetToByteArray(middle_packet), 'middlePacket');
+            }
 
+            let settings_packet = profile.PACKET_PROFILE_SETTINGS;
+
+            this.profile_state = {...this.profile_state, ...data};
+
+            for(const d of Object.entries(this.profile_state)) {
+                const param = d[0];
+                const value = d[1];
+                settings_packet = settings_packet.map((item) => {return item == param ? value : item;});
+            }
+            await this.sendCommand(utils.packetToByteArray(settings_packet), 'settingsPacket', 280);
+        }
     }
 
-    async setKeyColor(key : string, r: number, g: number, b: number) {
-        let packet = spectrum.SPECTRUM_PACKET;
+    async initKeys() {
+
+        const q = [{
+            name: "key",
+            message: "Which key?"
+        }];
+
+        let init_packet = utils.PACKET_INIT.map((item) => {return item == 'profile' ? this.profile_state.profile_num : item;});
+        let middle_packet = utils.PACKET_MIDDLE.map((item) => {return item == 'profile' ? this.profile_state.profile_num : item;});
+        let endPacket = spectrum.PACKET_SPECTRUM_END.map((item) => {return item == 'profile' ? this.profile_state.profile_num : item;});
+
+        
+        let packet2 = spectrum.PACKET_SPECTRUM_2.map((item) => {return item == 'profile' ? this.profile_state.profile_num : item;});
+        let keys  :{ [key: string]: number } ={}
+        const getKey = async (i: number) => {
+            let packet1 = spectrum.TEST_PACKET_SPECTRUM_1.map((item) => {return item == 'profile' ? this.profile_state.profile_num : item;});
+            packet1[i] = 255;
+            await this.sendCommand(utils.packetToByteArray(init_packet), 'initPacket');
+            await this.sendCommand(utils.packetToByteArray(middle_packet), 'middlePacket');
+            await this.sendCommand(utils.packetToByteArray(packet1), 'spectrum1Packet', 150);
+            await this.sendCommand(utils.packetToByteArray(packet2), 'spectrum2Packet'); 
+            await this.sendCommand(utils.packetToByteArray(endPacket), 'spectrumEndPacket', 280);
+            inquirer.prompt(q).then((ans) => {
+                if (ans['key']) {
+                    keys[ans['key']] = i;
+                }
+                i++;
+                if (i < 136) {
+                    getKey(i);
+                }
+            })
+        }
+        getKey(8)
+    }
+
+    async setKeyColor(key : string, r: number = 0, g: number = 0, b: number = 0) {
         if (!(key in spectrum.KEYS)) {
             console.error('Key is not properly set.')
         } else {
-            await this.sendCommand(utils.numArrayToHexString(packet), 'setKeyColor');
+            let init_packet = utils.PACKET_INIT.map((item) => {return item == 'profile' ? this.profile_state.profile_num : item;});
+            await this.sendCommand(utils.packetToByteArray(init_packet), 'initPacket');
+            if (this.profile_state.profile_num != profile.ProfileSelect.PC) {
+                let middle_packet = utils.PACKET_MIDDLE.map((item) => {return item == 'profile' ? this.profile_state.profile_num : item;});
+                await this.sendCommand(utils.packetToByteArray(middle_packet), 'middlePacket');
+            }
+            let endPacket = spectrum.PACKET_SPECTRUM_END.map((item) => {return item == 'profile' ? this.profile_state.profile_num : item;});
+
+            let packet1 = spectrum.TEST_PACKET_SPECTRUM_1.map((item) => {return item == 'profile' ? this.profile_state.profile_num : item;});
+            let packet2 = spectrum.PACKET_SPECTRUM_2.map((item) => {return item == 'profile' ? this.profile_state.profile_num : item;});
+
+            let key_pos = spectrum.KEYS[key];
+            packet1[key_pos] = r;
+            packet1[key_pos + 128] = g;
+            packet2[key_pos] = b;
+
+            await this.sendCommand(utils.packetToByteArray(packet1), 'spectrum1Packet', 150);
+            await this.sendCommand(utils.packetToByteArray(packet2), 'spectrum2Packet'); 
+            await this.sendCommand(utils.packetToByteArray(endPacket), 'spectrumEndPacket', 280);
         }
     }
 
-    private sendCommand(data : string, type: string) : Promise<number> {
+    private sendCommand(data : number[], type: string, to: number = 10) : Promise<number> {
         return new Promise((res, rej) => {
-            this.keyboard.open();
-            this.keyboard.controlTransfer(0x21, 0x09, 0x0307, 1, Buffer.from(data, 'hex'), (e) => {
-                if (e) {
-                    console.log('Error at', type);
-                    console.error(e);
-                }
-                this.keyboard.close();
-                res(1);
-            });
+            try {
+                setTimeout(() => {
+                    this.keyboard.sendFeatureReport(data);
+                    res(1);
+                }, to)
+            } catch(e) {
+                console.error('Error at', type);
+                rej(0);
+            }
         })
     }
 }
 
-let tesoro = new TesoroGramSE(USB.getDeviceList().filter(x => x.deviceDescriptor.idProduct == 0x2057)[0]);
-tesoro.changeProfile(profile.ProfileSelect.Profile2);
+const tesoro = new TesoroGramSE(new HID.HID(HID.devices().filter(x => x.path && x.productId == 0x2057 && x.interface == 1 && x.path.includes("col05"))[0].path!));
+
+tesoro.changeProfile(profile.ProfileSelect.PC);
+let data: ProfileState = {
+    r: 240,
+    g: 50,
+    b: 0,
+    effect: profile.Effect.Spectrum,
+    effect_color: profile.EffectColor.Static
+} 
+tesoro.setProfileSettings(data) 
+//tesoro.setKeyColor('Q', 255, 255, 0);
+tesoro.setKeyColor('A', 0, 255, 0);
+
+//tesoro.initKeys();
+
